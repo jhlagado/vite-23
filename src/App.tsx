d586195551602ce86742c7ebdb97e1c9bcae721d
John Hardy <jh@lagado.com>
2024-01-31 01:28:36 +1100

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import BullsEye from "./components/BullsEye";
 import SideBar from "./components/SideBar";
 
 export default function App() {
   return (
     <div className="flex">
       <SideBar />
-      {/* <BullsEye /> */}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('tailwindcss/colors') 
-
 export default {
   content: [
     "./index.html",
@@ -12,7 +10,16 @@ export default {
       colors: {
         primary: "#202225",
         secondary: "#5865f2",
-        gray: colors.trueGray,
+        gray: {
+          900: "#202225",
+          800: "#2f3136",
+          700: "#36393f",
+          600: "#4f545c",
+          400: "#d4d7dc",
+          300: "#e3e5e8",
+          200: "#ebedef",
+          100: "#f2f3f5",
+        },
       }
     },
   },
